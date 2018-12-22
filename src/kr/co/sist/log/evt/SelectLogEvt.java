871e7a4b90a0b04150d355325f5825c00b7ee89b
kimkunha <45455441+kimkunha@users.noreply.github.com>
2018-12-22 20:12:16 +0900

    Update SelectLogEvt.java

--- src/kr/co/sist/log/evt/SelectLogEvt.java
+++ src/kr/co/sist/log/evt/SelectLogEvt.java
@@ -29,7 +29,7 @@ import kr.co.sist.log.view.SelectLog;
 
 public class SelectLogEvt implements ActionListener {
 
-	// 1~6을 처리한 내용을 instance 변수에 저장해야 함
+	// 1~6�쓣 泥섎━�븳 �궡�슜�쓣 instance 蹂��닔�뿉 ���옣�빐�빞 �븿
 	private SelectLog sl;
 	private String filePath;
 	private Map<String, Integer> mapKey;
@@ -62,25 +62,14 @@ public class SelectLogEvt implements ActionListener {
 			try {
 				readLog();
 
-<<<<<<< HEAD
-				// readLog�� �о���� log�� ������ ����, instance������ ����
-				calMostFrequentKey();
-				calBrowserShare();
-				calCode403Share();
-
-				// ���â
-				new Result(this);
-				System.out.println("���â ������ ȣ��");
-=======
 				// readLog로 읽어들인 log의 내용을 가공, instance변수에 저장
 				calMostFrequentKey();
 				calBrowserShare();
 				calCode403Share();
-			
+
 				// 결과창
-				new Result(this, sl);
+				new Result(this);
 				System.out.println("결과창 생성자 호출");
->>>>>>> 7be9663ac978f11716dac0ca5a7f93cc982a5f3b
 
 			} catch (FileNotFoundException fnfe) {
 				fnfe.printStackTrace();
@@ -89,23 +78,12 @@ public class SelectLogEvt implements ActionListener {
 			}
 		}
 
-<<<<<<< HEAD
-		// jbView�� �ѹ� �̻� ���ȴٸ� JOptionPane.showMessageDialog�� �ٿ� ��� ���
-=======
 		// jbView가 한번 이상 눌렸다면 JOptionPane.showMessageDialog에 붙여 결과 출력
->>>>>>> 7be9663ac978f11716dac0ca5a7f93cc982a5f3b
 		if (e.getSource() == sl.getJbReport()) {
-			// jbView가 한번이상 눌렸을 때 수행되도록 구현(boolean flag로 구현)
 			if (reportFlag == true) {
-<<<<<<< HEAD
-				// "report ���"�� Component�� ��ü�ؾ� ��, Component�� ��ȯ�ϴ� method ���� ��
-
-				// ���� ��� FileDialog ����
-=======
 				// "report 출력"을 Component로 대체해야 함, Component를 반환하는 method 만들 것
 
 				// 파일 출력 FileDialog 구현
->>>>>>> 7be9663ac978f11716dac0ca5a7f93cc982a5f3b
 				try {
 					mkLogReport();
 				} catch (IOException e1) {
@@ -113,12 +91,12 @@ public class SelectLogEvt implements ActionListener {
 				}
 
 			} else {
-				JOptionPane.showMessageDialog(sl, "Log View를 먼저 수행하여 주세요.");
+				JOptionPane.showMessageDialog(sl, "Log View瑜� 癒쇱� �닔�뻾�븯�뿬 二쇱꽭�슂.");
 			}
 		}
 
 		if (e.getSource() == sl.getJbLineView()) {
-			// 시작,끝 라인이 입력됐을 때 해당 라인 수를 가져온다
+			// �떆�옉,�걹 �씪�씤�씠 �엯�젰�릱�쓣 �븣 �빐�떦 �씪�씤 �닔瑜� 媛��졇�삩�떎
 			start = Integer.parseInt(sl.getJtStartLine().getText());
 			end = Integer.parseInt(sl.getJtEndLine().getText());
 
@@ -138,28 +116,28 @@ public class SelectLogEvt implements ActionListener {
 
 	public void mkLogReport() throws IOException {
 <<<<<<< HEAD
-		// report, FileDialog SAVE, ����ϴ� method
+		// report, FileDialog SAVE, 출력하는 method
 
 =======
 
-		// report, FileDialog SAVE, 출력하는 method
+		// report, FileDialog SAVE, 異쒕젰�븯�뒗 method
 >>>>>>> 7be9663ac978f11716dac0ca5a7f93cc982a5f3b
 	}
 
 	public void calMostFrequentKey() {
-		// 가장 빈도수 높은 key(mostFrequentKey) 구하는 method
+		// 媛��옣 鍮덈룄�닔 �넂�� key(mostFrequentKey) 援ы븯�뒗 method
 	}
 
 	public void calMostFrequentHour() {
 <<<<<<< HEAD
-		// ���� �󵵼� ���� �ð��� ���ϴ� method
+		// 가장 빈도수 높은 시간을 구하는 method
 	}
 
 =======
-		// 가장 빈도수 높은 시간(mostFrequentHour) 구하는 method 
+		// 媛��옣 鍮덈룄�닔 �넂�� �떆媛�(mostFrequentHour) 援ы븯�뒗 method 
 	}
 	
-/////////////////////12.22 선의 코드 추가 (브라우저의 비율구해서 반환) 시작//////////////////////////////
+/////////////////////12.22 �꽑�쓽 肄붾뱶 異붽� (釉뚮씪�슦���쓽 鍮꾩쑉援ы빐�꽌 諛섑솚) �떆�옉//////////////////////////////
 >>>>>>> 7be9663ac978f11716dac0ca5a7f93cc982a5f3b
 	public void calBrowserShare() {
 		ArrayList<String> al = new ArrayList<String>();
@@ -167,7 +145,7 @@ public class SelectLogEvt implements ActionListener {
 		Iterator<String> ita = set.iterator();
 		Iterator<String> ita2 = set.iterator();
 
-//		System.out.println("모든넘버: " + requestNum);
+//		System.out.println("紐⑤뱺�꽆踰�: " + requestNum);
 //		System.out.println(mapBrowser);
 		for (int i = 0; i < browser.length; i++) {
 			mapBrowserShare.put(ita2.next(), String.format("%4.2f", ((mapBrowser.get(ita.next()) / (double) requestNum) * 100)));
@@ -177,7 +155,7 @@ public class SelectLogEvt implements ActionListener {
 	}
 <<<<<<< HEAD
 =======
-/////////////////////12.22 선의 코드 추가 (브라우저의 비율구해서 반환) 끝//////////////////////////////
+/////////////////////12.22 �꽑�쓽 肄붾뱶 異붽� (釉뚮씪�슦���쓽 鍮꾩쑉援ы빐�꽌 諛섑솚) �걹//////////////////////////////
 
 >>>>>>> 7be9663ac978f11716dac0ca5a7f93cc982a5f3b
 
@@ -187,11 +165,11 @@ public class SelectLogEvt implements ActionListener {
 
 	public void selectLog() {
 <<<<<<< HEAD
-		// �о���� log������ ��θ� �����ϴ� method
-		FileDialog fd = new FileDialog(sl, "log ���� ����", FileDialog.LOAD);
-=======
 		// 읽어들인 log파일의 경로를 저장하는 method
-		FileDialog fd = new FileDialog(sl, "log �뙆�씪 �꽑�깮", FileDialog.LOAD);
+		FileDialog fd = new FileDialog(sl, "log 파일 선택", FileDialog.LOAD);
+=======
+		// �씫�뼱�뱾�씤 log�뙆�씪�쓽 寃쎈줈瑜� ���옣�븯�뒗 method
+		FileDialog fd = new FileDialog(sl, "log 占쎈솁占쎌뵬 占쎄퐨占쎄문", FileDialog.LOAD);
 >>>>>>> 7be9663ac978f11716dac0ca5a7f93cc982a5f3b
 		fd.setVisible(true);
 
@@ -210,8 +188,8 @@ public class SelectLogEvt implements ActionListener {
 			while ((temp = br.readLine()) != null) {
 
 				requestNum++;
-				// 선택된 파일의 내용을 한 줄씩 읽어들임
-				// 읽어들이는 내용을 처리하는건 따로 method 만들어서 처리할 것
+				// �꽑�깮�맂 �뙆�씪�쓽 �궡�슜�쓣 �븳 以꾩뵫 �씫�뼱�뱾�엫
+				// �씫�뼱�뱾�씠�뒗 �궡�슜�쓣 泥섎━�븯�뒗嫄� �뵲濡� method 留뚮뱾�뼱�꽌 泥섎━�븷 寃�
 				if (start == 0 && end == 0) {
 					countKey(temp);
 					countBrowser(temp);
@@ -233,22 +211,22 @@ public class SelectLogEvt implements ActionListener {
 	}
 
 <<<<<<< HEAD
-	/////////////////////////// 12-22-2018 Ű�� ī��Ʈ �޼ҵ� ����(����)
+	/////////////////////////// 12-22-2018 키값 카운트 메소드 구현(건하)
 	/////////////////////////// ///////////////////////////////
 	public void countKey(String temp) {
-		// 1. �ִ� ��� Key�� �̸��� Ƚ���� ���ϴ� method
+		// 1. 최다 사용 Key의 이름과 횟수를 구하는 method
 //		String[] str=null;
 //		Integer[] intValue=null;
 		Set<String>setKey=new HashSet<>();
 
 		if (temp.contains("key")) {
 //			for(int i=0;i<requestNum;i++) {
-			String str = temp.substring(temp.indexOf("=") + 1, temp.indexOf("&")); // ��ȯ���� int���̶� ��ȯ���� ������� �Ѵ�.
+			String str = temp.substring(temp.indexOf("=") + 1, temp.indexOf("&")); // 반환형이 int형이라서 반환형을 맞춰줘야 한다.
 			
 			for(int i=0; i<str.length(); i++) {
 				setKey.add(String.valueOf(str));
 			}
-			System.out.println(setKey); // Ű�� ���ϱ�
+			System.out.println(setKey); // 키값 구하기
 			
 			
 //		for(int i=0; i<str.length(); i++) {
@@ -271,11 +249,11 @@ public class SelectLogEvt implements ActionListener {
 		
 		
 	// System.out.println(str);
-	// ���� Ű������ �� �Ҵ��ϱ�.
+	// 구한 키값으로 값 할당하기.
 
-//		Set<String>allkeys=mapKey.keySet();	//��� Ű ���
+//		Set<String>allkeys=mapKey.keySet();	//모든 키 얻기
 //		System.out.println(allkeys);
-//		Iterator<String>ita=allkeys.iterator();	//��� �� ���
+//		Iterator<String>ita=allkeys.iterator();	//모든 값 얻기
 //		Integer count=0;
 //		while(ita.hasNext()) {
 //			count=mapKey.get(ita.next());
@@ -283,24 +261,24 @@ public class SelectLogEvt implements ActionListener {
 //		}
 
 	}// countKey
-		/////////////////////////// 12-22-2018 Ű�� ī��Ʈ �޼ҵ� ��
+		/////////////////////////// 12-22-2018 키값 카운트 메소드 끝
 		// ///////////////////////////////
 
 	public void countBrowser(String temp) {
 
-		// 2. �������� ���� Ƚ�� ���ϴ� method, ���� ���ϱ�(����)
+		// 2. 브라우저별 접속 횟수 구하는 method, 비율 구하기(아직)
 	}
 =======
 	public void countKey(String temp) {
-		// 1. 최다 사용 Key의 이름과 횟수를 구하는 method
+		// 1. 理쒕떎 �궗�슜 Key�쓽 �씠由꾧낵 �슏�닔瑜� 援ы븯�뒗 method
 	}
 
-//////////////////////12.22 선의 추가 코드(브라우저,카운터 mapBrowser에 넣기) 시작 ////////////////////////////
+//////////////////////12.22 �꽑�쓽 異붽� 肄붾뱶(釉뚮씪�슦��,移댁슫�꽣 mapBrowser�뿉 �꽔湲�) �떆�옉 ////////////////////////////
 	private String[] browser = { "opera", "ie", "firefox", "Chrome", "Safari" };
 	private int[] browserCnt = new int[browser.length];
 
 	public void countBrowser(String temp) {
-		// 2. 브라우저별 접속 횟수 구하는 method, 비율 구하기(아직)
+		// 2. 釉뚮씪�슦��蹂� �젒�냽 �슏�닔 援ы븯�뒗 method, 鍮꾩쑉 援ы븯湲�(�븘吏�)
 //		System.out.println(temp);
 		int count = 0;
 //		System.out.println("temp :"+temp  );
@@ -313,24 +291,24 @@ public class SelectLogEvt implements ActionListener {
 		} // end for
 //		System.out.println("requestNum = " +requestNum+", "+mapBrowser);
 	}// countBrowser
-/////////////////////12.22 선의 추가 코드(브라우저,카운터 mapBrowser에 넣기) 끝////////////////////////////
+/////////////////////12.22 �꽑�쓽 異붽� 肄붾뱶(釉뚮씪�슦��,移댁슫�꽣 mapBrowser�뿉 �꽔湲�) �걹////////////////////////////
 >>>>>>> 7be9663ac978f11716dac0ca5a7f93cc982a5f3b
 
 	public void countHttpStatusCode(String temp) {
-		// 3. 서비스를 성공적으로 수행한 횟수, 실패(404) 횟수
-		// 6. 비정상적인 요청(403)이 발생한 횟수 구하는 method, 비율 구하기 method는 calBrowserShare()로 구현
+		// 3. �꽌鍮꾩뒪瑜� �꽦怨듭쟻�쑝濡� �닔�뻾�븳 �슏�닔, �떎�뙣(404) �슏�닔
+		// 6. 鍮꾩젙�긽�쟻�씤 �슂泥�(403)�씠 諛쒖깮�븳 �슏�닔 援ы븯�뒗 method, 鍮꾩쑉 援ы븯湲� method�뒗 calBrowserShare()濡� 援ы쁽
 	}
 <<<<<<< HEAD
 
 	public void countRequestHour(String temp) {
-		// 4. ��û �ð��� Ƚ���� ���ϴ� method.
+		// 4. 요청 시간별 횟수를 구하는 method.
 
 		Map<String, Integer> map = new HashMap<String, Integer>();
 
 =======
 
 	public void countRequestHour(String temp) {
-		// 4. 요청 시간별 횟수를 구하는 method.
+		// 4. �슂泥� �떆媛꾨퀎 �슏�닔瑜� 援ы븯�뒗 method.
 		Map<String, Integer>map=new HashMap<String,Integer>();
 		
 >>>>>>> 7be9663ac978f11716dac0ca5a7f93cc982a5f3b
